c2283f9cfa7a0cc9f538565323397e889572ba87
Reynaeris <reynakest@gmail.com>
2023-12-06 23:24:22 -0500

    Finished quiz 3, fixed bad references

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,13 +18,13 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard <T>{
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -39,12 +39,12 @@ mod tests {
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
             grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
-            student_age: 12,
+            student_name: "Xaden Riorsin".to_string(),
+            student_age: 23,
         };
         assert_eq!(
             report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1"
+            "Xaden Riorsin (23) - achieved a grade of 2.1"
         );
     }
 
@@ -52,13 +52,13 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
-            student_age: 11,
+            grade: "A+",
+            student_name: "Violet Sorrengail".to_string(),
+            student_age: 21,
         };
         assert_eq!(
             report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
+            "Violet Sorrengail (21) - achieved a grade of A+"
         );
     }
 }
